202e7791ef478d4a51eacbd03ea510820c2be355
Oleksii Tsoniev <oleksii_tsoniev@epam.com>
2017-11-07 16:20:37 +0200

    Initial commit

---
+++ Task Tracker/Classes/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Task Tracker
+//
+//  Created by Oleksii Tsoniev on 11/7/17.
+//  Copyright © 2017 Oleksii Tsoniev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
